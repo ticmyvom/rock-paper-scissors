93b6cc41f3ffaeef2489e1229d382377298ffaea
anhngo <63425188+ticmyvom@users.noreply.github.com>
2024-01-07 22:48:29 -0800

    Add playRound(playerSelection, computerSelection)
    This function returns the result of the round depending on playerSelection
    and computerSelection.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-console.log("Hello World");
-
 /* Convert 0-2 to 'Rock, 'Paper', Scissors' */
 function numToTool(number) {
     switch (number) {
-        case 0: return 'Rock';
-        case 1: return 'Paper';
-        case 2: return 'Scissors'
+        case 0: return 'rock';
+        case 1: return 'paper';
+        case 2: return 'scissors'
     }
 }
 
@@ -14,4 +12,29 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3); // returns a random integer from 0 to 2
     return numToTool(choice);
 }
-console.log(getComputerChoice());
+
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+function playRound(playerSelection, computerSelection) {
+    let announcement;
+    console.log("Inside playRound: ", computerSelection, playerSelection);
+    playerSelection = playerSelection.trim().toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "TIES";
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors' 
+            || playerSelection === 'scissors' && computerSelection === 'paper'
+            || playerSelection === 'paper' && computerSelection === 'rock') {
+        playerSelection = capitalizeFirstLetter(playerSelection);
+        announcement = `You win! ${playerSelection} beats ${computerSelection}.`;
+    } else {
+        announcement = `You lose, ${computerSelection} beats ${playerSelection}.`;
+    }
+    return announcement;
+}
+
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
