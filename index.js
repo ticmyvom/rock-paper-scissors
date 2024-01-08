1284eefd0bb5694f524c067f06391d7ff23eed8f
anhngo <63425188+ticmyvom@users.noreply.github.com>
2024-01-08 11:14:04 -0800

    Add game() that utilizes playRound()
    game() determine the winner by playing a game-of-five.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function capitalizeFirstLetter(string) {
 function playRound(playerSelection, computerSelection) {
     let announcement;
     let result;
-    console.log("Inside playRound: ", computerSelection, playerSelection);
+    // console.log("Inside playRound: ", computerSelection, playerSelection);
     playerSelection = playerSelection.trim().toLowerCase();
     if (playerSelection === computerSelection) {
         return "TIES";
@@ -30,16 +30,54 @@ function playRound(playerSelection, computerSelection) {
             || playerSelection === 'paper' && computerSelection === 'rock') {
         playerSelection = capitalizeFirstLetter(playerSelection);
         announcement = `You win! ${playerSelection} beats ${computerSelection}.`;
-        result = true;
+        result = true; // player wins
     } else {
         announcement = `You lose, ${computerSelection} beats ${playerSelection}.`;
-        result = false;
+        result = false; // computer wins
     }
     console.log(announcement);
     return result;
 }
 
 // TEST: playRound()
-const playerSelection = "paper";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// const playerSelection = "paper";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+
+/* Play a best-of-five game that keeps score and return the winner at the end */
+function game() {
+    // keep track of score: 1 for player, 1 for computer
+    // play 5 round and determine the winner:
+    // for loop 5 times, call playRound() 
+    //      prompt user for selection
+    //      while TIES, re-prompt the user and call playRound()
+    //      else, a winner is determined so we increase the score
+    // announce the winner after the for loop
+
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, Paper, Scissors! What is your choice?", "rock");
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+        while (result === "TIES") {
+            console.log("TIES");
+            playerSelection = prompt("Ties! Let's try again. What is your choice?", "rock");
+            computerSelection = getComputerChoice();
+            result = playRound(playerSelection, computerSelection);
+        }
+
+        if (result) {
+            playerScore += 1;
+        } else {
+            computerScore += 1;
+        }
+        console.log(`End of round result: player has won ${playerScore} out of ${i + 1} rounds.`);
+    } // end for
+    let result = (playerScore > computerScore) ? "Player won!" : "Computer won.";
+    console.log(result);
+}
+
+game();
+
+// TODO: possibly group log messages for better organization
